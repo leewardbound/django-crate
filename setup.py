79a30ce3af0c0a6a404bdb5ced75d20b28e27d92
leeward bound <l@lwb.co>
2016-08-05 11:03:55 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django_crate',
+      version='0.1.33',
+      description='Django database backend for crate.io',
+      author='Leeward Bound',
+      author_email='l@lwb.co',
+      url='https://github.com/linked/django-crate',
+      packages=['django_crate',],
+     )
