a82f823feb8102025a6ae0d489ee114dad587696
Christian Haudum <christian.haudum@gmail.com>
2016-12-14 15:55:02 +0100

    re-implemented introspection queries based on postgres

--- django_crate/backend/introspection.py
+++ django_crate/backend/introspection.py
@@ -1,9 +1,17 @@
-# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from collections import namedtuple
+
 from django.db.backends.base.introspection import (
     BaseDatabaseIntrospection, FieldInfo, TableInfo,
 )
+from django.utils.encoding import force_text
+
+FieldInfo = namedtuple('FieldInfo', FieldInfo._fields + ('default',))
+
 
 class DatabaseIntrospection(BaseDatabaseIntrospection):
+    # Maps type codes to Django Field types.
     data_types_reverse = {
         "boolean": "BooleanField",
         "byte": "SmallIntegerField",
@@ -13,64 +21,80 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
         "float": "FloatField",
         "double": "FloatField",  # no double type in python
         "timestamp": "DateTimeField",
-        "ip": "CharField",
+        "ip": "IPAddressField",
         "string": "CharField",
         # TODO: object and array
     }
 
-    def get_table_list(self, cursor):
-        tables = []
-        db = self.connection.settings_dict['NAME'] or 'doc'
-        cursor.execute(
-            "select table_name from information_schema.tables "
-            "where table_schema='%s' order by table_name"%db)
-        for table_name in cursor.fetchall():
-            if isinstance(table_name, list):
-                table_name = table_name[0]
-            tables.append(table_name)
-        return [TableInfo(t, 't') for t in tables]
+    def get_field_type(self, data_type, description):
+        # TODO
+        return super(DatabaseIntrospection, self).get_field_type(data_type, description)
 
-    def table_names(self, cursor):
-        output = super(DatabaseIntrospection, self).table_names(cursor)
-        return [isinstance(o, tuple) and o[0] or o for o in output]
+    def get_table_list(self, cursor):
+        cursor.execute("""
+            SELECT table_name AS relname, 't' AS relkind
+            FROM information_schema.tables
+            WHERE table_schema NOT IN ('sys', 'blob', 'information_schema', 'pg_catalog')
+              AND table_schema = CURRENT_SCHEMA()""")
+        return [TableInfo(*row) for row in cursor.fetchall()]
+
+    def get_table_description(self, cursor, table_name):
+        cursor.execute("""
+            SELECT column_name, is_nullable, NULL AS column_default
+            FROM information_schema.columns
+            WHERE table_name = ?
+              AND table_schema = CURRENT_SCHEMA()""", [table_name])
+        field_map = {line[0]: line[1:] for line in cursor.fetchall()}
+        cursor.execute('SELECT * FROM {} LIMIT 1'.format(self.connection.ops.quote_name(table_name)))
+        return [
+            FieldInfo(*(
+                (force_text(line[0]),) +
+                line[1:6] +
+                (field_map[force_text(line[0])][0] == 'YES', field_map[force_text(line[0])][1])
+            )) for line in cursor.description
+        ]
 
     def sequence_list(self):
-        """sequences not supported"""
         return []
+
+    def get_relations(self, cursor, table_name):
+        return {}
 
     def get_key_columns(self, cursor, table_name):
         return []
 
     def get_indexes(self, cursor, table_name):
+        cursor.execute("""SELECT column_name, column_name = any(constraint_name) AS is_primary
+        FROM information_schema.columns col,
+             information_schema.table_constraints con
+        WHERE con.table_name = col.table_name
+          AND col.table_name = ?
+          AND col.table_schema = CURRENT_SCHEMA()""", [table_name])
         indexes = {}
-        cursor.execute(
-            "select constraint_name from information_schema.table_constraints "
-            "where table_schema='doc' and table_name='{}'".format(table_name)
-        )
-        for constraints in cursor.fetchall():
-            # only handle single column indices
-            if len(constraints) == 1:
-                colname = constraints[0]
-                indexes[colname] = {
-                    'primary_key': True,
-                    'unique': True
-                }
+        for col_name, is_primary in cursor.fetchall():
+            indexes[col_name] = {
+                'primary_key': is_primary,
+                'unique': True
+            }
         return indexes
 
     def get_constraints(self, cursor, table_name):
-        result = {}
-        cursor.execute(
-            "select constraint_name from information_schema.table_constraints "
-            "where table_schema='doc' and table_name='{}'".format(table_name)
-        )
-        for constraints in cursor.fetchall():
-            constraint_name = "__".join([table_name] + constraints[0])
-            result[constraint_name] = {
-                "columns": constraints[0],
-                "primary_key": True,
-                "unique": True,
-                "foreign_key": None,
-                "check": False,
-                "index": False
-            }
-        return result
+        cursor.execute("""
+        SELECT constraint_name, constraint_type
+        FROM information_schema.table_constraints
+        WHERE table_name = ?
+          AND table_schema = CURRENT_SCHEMA()""", [table_name])
+        constraints = {}
+        for columns, kind in cursor.fetchall():
+            name = "__".join([table_name] + columns)
+            # If we're the first column, make the record
+            if name not in constraints:
+                constraints[constraint] = {
+                    "columns": columns,
+                    "primary_key": kind.lower() == "primary_key",
+                    "unique": kind.lower() in ["primary_key", "unique"],
+                    "foreign_key": None,
+                    "check": False,
+                    "index": False,
+                }
+        return constraints
