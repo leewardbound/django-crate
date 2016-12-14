0f998352368bcd52bad7c07e4b9183f1455fade7
Christian Haudum <christian.haudum@gmail.com>
2016-12-14 09:38:15 +0100

    updated example settings to reflect new psycopg connection params

--- example/test_settings.py
+++ example/test_settings.py
@@ -1,25 +1,31 @@
 import logging
+from django.core import mail
+from django.core.mail.backends.base import BaseEmailBackend
+
 logging.disable(logging.CRITICAL)
+
 DEBUG = False
-DATABASES = {'default': {
-                    'ENGINE': 'django.db.backends.sqlite3',
-                    'NAME': '.sqlite-test-db',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '.sqlite-test-db',
     },
     'crate': {
         'ENGINE': 'django_crate.backend',
-        'SERVERS': ['test_crate:4200',],
+        'HOST': 'localhost',
+        'PORT': 5432,
     },
 }
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
-from django.core.mail.backends.base import BaseEmailBackend
-from django.core import mail
+
 class TestEmailBackend(BaseEmailBackend):
     def send_messages(self, messages):
         mail.outbox.extend(messages)
         return len(messages)
+
 EMAIL_BACKEND='conf.TestEmailBackend'
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
